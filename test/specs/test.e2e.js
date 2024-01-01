920513ad7c9ee51eef8736d31a6c01ff14194ad7
starred_gemini <elizaveta.19.bk@gmail.com>
2024-01-02 01:54:52 +0200

    changed element find syntax

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,20 +1,30 @@
 // test.e2e.ts
+import { By, until, WebDriver } from "appium-webdriver";
 import Page from "../pageobjects/page.js";
 
 const myPage = new Page();
+WebDriver = driver;
 
 describe("My Login application", () => {
   it("should divide numbers properly", async () => {
-    const btn7 = await browser.$("id=com.google.android.calculator:id/digit_7");
-    const btnDiv = await browser.$(
-      "id=com.google.android.calculator:id/op_div"
+    const btn7 = driver.findElement(
+      By.id("com.google.android.calculator:id/digit_7")
     );
-    const btn5 = await browser.$("id=com.google.android.calculator:id/digit_5");
-    const btnEq = await browser.$("id=com.google.android.calculator:id/eq");
-    const result = await browser.$(
-      "id=com.google.android.calculator:id/result_final"
+    const btnDiv = driver.findElement(
+      By.id("com.google.android.calculator:id/op_div")
     );
-    const btnClr = await browser.$("id=com.google.android.calculator:id/clr");
+    const btn5 = driver.findElement(
+      By.id("id=com.google.android.calculator:id/digit_5")
+    );
+    const btnEq = driver.findElement(
+      By.id("id=com.google.android.calculator:id/eq")
+    );
+    const result = driver.findElement(
+      By.id("id=com.google.android.calculator:id/result_final")
+    );
+    const btnClr = driver.findElement(
+      By.id("id=com.google.android.calculator:id/clr")
+    );
 
     await btn7.waitForDisplayed({ timeout: 10000 });
     await btn7.click();
@@ -104,6 +114,33 @@ describe("My Login application", () => {
     expect(await result.getText()).toEqual("0");
   });
 });
+
+// import { By, until, WebDriver } from "appium-webdriver";
+// import { expect, browser } from "@wdio/globals";
+// import Page from "../pageobjects/page.js";
+// const myPage = new Page();
+
+// describe("My Login application", () => {
+//   it("should sign up with valid credentials", async () => {
+//     const btn7 = WebDriver.findElement(
+//       By.id("com.google.android.calculator:id/digit_7")
+//     );
+//     const btnDiv = WebDriver.findElement(
+//       By.id("com.google.android.calculator:id/op_div")
+//     );
+//     const btn5 = WebDriver.findElement(
+//       By.id("com.google.android.calculator:id/digit_5")
+//     );
+//     // await browser.pause(5000);
+//     await myPage.clickElement(btn7);
+//     await myPage.clickElement(btnDiv);
+//     await myPage.clickElement(btn5);
+//     // expect(await entry.isSignUpButtonDisplayed(signUpBtnIos)).true;
+//     // expect(await entry.getSignUpButtonText(signUpBtnIos)).toEqual("Sign up"); // element has no text
+//     // await entry.clickSignUpButton(signUpBtnIos);
+//     await browser.pause(5000);
+//   });
+// });
 
 // =========================
 
